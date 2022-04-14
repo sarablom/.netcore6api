8e172d96a3490364eb0ad5d95405fae0529d19f3
Sara Blom <sara.cecilia.blom@gmail.com>
2022-04-14 14:26:26 +0200

    add routing

--- CityInfo.Api/Controllers/CitiesController.cs
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace CityInfo.Api.Controllers
 {
     [ApiController]
+    [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
+        [HttpGet]
         public JsonResult GetCities()
         {
             return new JsonResult(
