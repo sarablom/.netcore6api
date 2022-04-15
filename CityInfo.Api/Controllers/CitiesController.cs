3203da6780faac95a4a182a817d64036782acf9f
Sara Blom <sara.cecilia.blom@gmail.com>
2022-04-15 07:40:54 +0200

    add model and datastore

--- CityInfo.Api/Controllers/CitiesController.cs
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -9,11 +9,7 @@ namespace CityInfo.Api.Controllers
         [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(
-                new List<object> {
-                    new {Id = 1, Name = "New York City", Description = "The one with that big park."},
-                    new {Id = 2, Name = "Antwerp", Description = "The one with the cathedral that was never really finished." }
-                });
+            return new JsonResult(CitiesDataStore.Current.Cities);
         }
     }
 }
---
+++ CityInfo.Api/Models/CityDto.cs
@@ -0,0 +1,9 @@
+namespace CityInfo.Api.Controllers.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
